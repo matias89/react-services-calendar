5220cc16bb59c4659d853e3665e5feb7a6bceb63
Matías Aybar <matias.aybar@globant.com>
2020-07-09 18:29:16 -0300

    Test to include repo to other project

---
+++ build/precache-manifest.9d65e0e94ba2ee934dbf72c5c021b519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7528dfd72dce40f884e21e3ced20c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6b155ed3969284479cb",
+    "url": "/static/css/main.0f302df6.chunk.css"
+  },
+  {
+    "revision": "83f242e465539f30c2d6",
+    "url": "/static/js/2.6aa8638d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6aa8638d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b155ed3969284479cb",
+    "url": "/static/js/main.73246a61.chunk.js"
+  },
+  {
+    "revision": "94d9b25e89fc4d01221c",
+    "url": "/static/js/runtime-main.8463a8a9.js"
+  }
+]);
